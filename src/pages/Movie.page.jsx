1ae55020541af04924b629ae21100ddf544c990b
Zubair-cell <zubair.malik30031@gmail.com>
2022-09-25 04:56:06 +0530

    Completed movie page and payment gateway

--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react'
 import MovieLayoutHOC from '../layout/Movie.layout';
-import { Form, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from 'axios';
 import { MovieContext } from "../context/Movie.context";
 import Slider from 'react-slick';
 import { FaCcVisa, FaCcApplePay } from "react-icons/fa";
 import PosterSlider from '../components/PosterSlider/PosterSlider.Component';
 import MovieHero from '../components/MovieHero/MovieHero.Component';
+import Cast from "../components/Cast/Cast.Component";
 
 const MoviePage = () => {
 
@@ -54,7 +55,37 @@ const MoviePage = () => {
         requestMovie();
     }, [id]);
 
-    const settingCast = {};
+    const settingCast = {
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
 
     const settings = {
         infinite: false,
@@ -143,6 +174,20 @@ const MoviePage = () => {
                 </div>
 
                 {/* cast Slider*/}
+                <div className='my-8'>
+                    <h2 className='text-gray-800 font-bold text-2xl mb-4'>
+                        Cast and Crew
+                    </h2>
+                    <Slider {...settingCast}>
+                        {cast.map((castData) => (
+                            <Cast
+                                image={castData.profile_path}
+                                castName={castData.original_name}
+                                role={castData.character}
+                            />
+                        ))}
+                    </Slider>
+                </div>
 
                 <div className='my-8'>
                     <hr />
